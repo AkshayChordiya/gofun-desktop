4a96787623206efc513613994794068e9b88a1ae
Akshay Chordiya <akshaychordiya2@gmail.com>
2017-03-02 15:24:03 +0530

    Bumped to v1.1

--- gofun.go
+++ gofun.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/andlabs/ui"
+	"github.com/howeyc/gopass"
 	"github.com/ricardolonga/jsongo"
 	"io/ioutil"
 	"net/http"
@@ -34,48 +35,50 @@ func main() {
 // Starts the countdown if the user is already logged in else prompts the user to
 // authenticate with the server
 func StartTime() {
+	fmt.Println("Welcome to GoFun")
 	token := GetToken()
 	if token != "" {
-		// Start time here
-		fmt.Println("Welcome to GoFun")
-		values := jsongo.Object().Put("in_time", GetCurrentTimestamp())
-		request, _ := http.NewRequest("POST", START_TIME_API_URL, bytes.NewBufferString(values.String()))
-		request.Header.Add("Content-Type", "application/json")
-		request.Header.Add("Authorization", "Token "+token)
-		client := &http.Client{}
-		response, _ := client.Do(request)
-		if response.StatusCode == 201 {
-			// We are good to go!
-			fmt.Println("Commencing countdown ⏰ to Go Home 🏠")
-			hour, min := 7, 30
-			endTime := GetWorkEndTime(hour, min)
-
-			fmt.Println("Going Home at", endTime.Format(time.Kitchen), "after", endTime.Sub(time.Now()))
-
-			// Create new ticker
-			ticker := time.NewTicker(time.Minute * 1)
-			go StartTimeTicker(*time.NewTicker(time.Minute * 1), endTime)
-
-			// Let's keep the app running till working hour is complete
-			<-time.After(time.Duration(hour*60+min) * time.Minute)
-
-			// Completion hours are complete, let's stop the timer
-			ticker.Stop()
-			ShowGoHomeWindow(GO_FUN_MESSAGE)
-		}
+		beginTime(token)
 	} else {
-		fmt.Println("Welcome to GoFun")
-		success := AuthenticateUser()
-		if success {
-			StartTime()
-		}
+		token = AuthenticateUser()
+		beginTime(token)
+	}
+}
+
+// Send the time start request to server and start the countdown
+func beginTime(token string) {
+	// Start time here
+	values := jsongo.Object().Put("in_time", GetCurrentTimestamp())
+	request, _ := http.NewRequest("POST", START_TIME_API_URL, bytes.NewBufferString(values.String()))
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Authorization", "Token "+token)
+	client := &http.Client{}
+	response, _ := client.Do(request)
+	if response.StatusCode == 201 {
+		// We are good to go!
+		fmt.Println("Commencing countdown ⏰ to Go Home 🏠")
+		hour, min := 7, 30
+		endTime := GetWorkEndTime(hour, min)
+
+		fmt.Println("Going Home at", endTime.Format(time.Kitchen), "after", endTime.Sub(time.Now()))
+
+		// Create new ticker
+		ticker := time.NewTicker(time.Minute * 1)
+		go StartTimeTicker(*time.NewTicker(time.Minute * 1), endTime)
+
+		// Let's keep the app running till working hour is complete
+		<-time.After(time.Duration(hour*60+min) * time.Minute)
+
+		// Completion hours are complete, let's stop the timer
+		ticker.Stop()
+		ShowGoHomeWindow(GO_FUN_MESSAGE)
 	}
 }
 
 // Authenticates the user with the entered credentials to the server and
 // stores the auth-token returned by server into the file.
 // Returns true if everything goes as planned else false.
-func AuthenticateUser() bool {
+func AuthenticateUser() string {
 	var token Token
 	// Read user credentials
 	username, password := ReadUserCredentials()
@@ -89,11 +92,8 @@ func AuthenticateUser() bool {
 	json.Unmarshal(body, &token)
 	// Persist the object aka Token into file
 	file, _ := os.Create("../token")
-	_, err := file.WriteString(token.Token)
-	if err != nil {
-		return true
-	}
-	return false
+	file.WriteString(token.Token)
+	return token.Token
 }
 
 // Show dialog to notify user about completion of his/her working hours
@@ -126,10 +126,11 @@ func ShowGoHomeWindow(message string) {
 func ReadUserCredentials() (username string, password string) {
 	// Reading the user credentials
 	consoleReader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter username: ")
+	fmt.Print("Enter email address: ")
 	username, _ = consoleReader.ReadString('\n')
-	fmt.Println("Enter password: ")
-	password, _ = consoleReader.ReadString('\n')
+	fmt.Print("Enter password: ")
+	bpassword, _ := gopass.GetPasswdMasked()
+	password = string(bpassword)
 	return
 }
 
